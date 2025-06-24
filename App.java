1480c2d20c4bff5261ecb66c5440be0044652e49
Your Name <you@example.com>
2025-06-24 19:36:01 -0300

    Generación del árbol sintáctico en DOT y PNG

--- App.java
+++ App.java
@@ -1,8 +1,14 @@
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
+import org.antlr.v4.runtime.tree.Trees;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class App {
-    
+
     public static void main(String[] args) throws Exception {
         CharStream input = CharStreams.fromFileName("programaEjemplo.cpp");
 
@@ -19,14 +25,45 @@ public class App {
         System.out.println("Arbol sintáctico:");
         System.out.println(tree.toStringTree(parser));
 
+        // Exportar a archivo DOT
+        exportParseTreeToDot(tree, parser, "arbol.dot");
+        System.out.println("Archivo DOT generado como arbol.dot");
+
         // Análisis semántico
         ParseTreeWalker walker = new ParseTreeWalker();
         SemanticAnalyzer semanticAnalyzer = new SemanticAnalyzer();
         walker.walk(semanticAnalyzer, tree);
 
-        // Mostrar resultado del análisis semántico
         if (!semanticAnalyzer.hasErrors()) {
             System.out.println("Análisis semántico exitoso.");
         }
+    }
+
+    public static void exportParseTreeToDot(ParseTree tree, Parser parser, String filename) throws Exception {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
+        writer.write("digraph G {\n");
+        writer.write("  node [shape=box];\n");
+        exportNode(tree, parser, writer, 0, new AtomicInteger(1));
+        writer.write("}\n");
+        writer.close();
+    }
+
+    private static int exportNode(ParseTree node, Parser parser, BufferedWriter writer, int id, AtomicInteger counter) throws IOException {
+        String nodeName = "node" + id;
+        String label = escapeLabel(Trees.getNodeText(node, parser));
+        writer.write("  " + nodeName + " [label=\"" + label + "\"];\n");
+
+        for (int i = 0; i < node.getChildCount(); i++) {
+            int childId = counter.getAndIncrement();
+            String childName = "node" + childId;
+            writer.write("  " + nodeName + " -> " + childName + ";\n");
+            exportNode(node.getChild(i), parser, writer, childId, counter);
+        }
+
+        return id;
+    }
+
+    private static String escapeLabel(String text) {
+        return text.replace("\"", "\\\"");
     }
 }
